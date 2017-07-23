e1e7ab6655ce3f74d23e8d1b728f7c5686d5bc6d
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-07-23 22:53:11 +0530

    Add files via upload

---
+++ spring-flyway-db-migration/target/classes/db/migration/V1__init.sql
@@ -0,0 +1,13 @@
+
+
+create table customer(
+	
+	customer_id BIGINT PRIMARY KEY,
+	customer_name varchar(255)
+	
+);
+
+insert into customer values(100,'Barath');
+insert into customer values(101,'Senthil');
+insert into customer values(102,'Thiyagu');
+
